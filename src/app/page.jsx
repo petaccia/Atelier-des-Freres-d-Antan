741b359f0e950d9ab2114238b3f018c64504d96e
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-18 13:53:11 +0100

    feat: create history section. Add professional history section with responsive design. Include image with dynamic aspect ratio. Implement modern layout and typography

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import CarouselHome from "@/components/carousel/CarouselHome";
 import ValueSection from "@/components/sections/home/ValueSection/ValueSection";
 import EssentialsSection from "@/components/sections/home/essentialsSection/EssentialsSection";
+import HistorySection from "@/components/sections/home/historySection/HistorySection";
 import Testimonials from "@/components/sections/home/testimonial/Testimonial";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <CarouselHome />
       <EssentialsSection />
       <ValueSection />
+      <HistorySection />
       <Testimonials />
     </div>
   );
