da2974d5e316b8c13aecd86efbb3da27c34ef3a6
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-20 12:00:40 +0100

    Create phylosophy component with the style. Add in the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import HistorySection from "@/components/sections/home/historySection/HistorySec
 import Testimonials from "@/components/sections/home/testimonial/Testimonial";
 import ServicesSection from "@/components/sections/home/servicesSection/ServicesSection";
 import PartnerSection from "@/components/sections/home/PartnersSection/PartnersSection";
+import PhilosophySection from "@/components/sections/home/philosophySection/PholosophySection";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <HistorySection />
       <ServicesSection />
       <Testimonials />
+      <PhilosophySection />
       <PartnerSection />
     </div>
   );
