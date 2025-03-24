3770996598d9d211873d847cd92c8a94f973a231
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-24 11:15:03 +0100

    feat: add ShopAddres component - Created ShopAddress component to centralize shop address - Updated CarouselHome to use ShopAddress - Improved code reusability and maintainability

--- src/components/carousel/CarouselHome.jsx
+++ src/components/carousel/CarouselHome.jsx
@@ -7,9 +7,10 @@ import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import { carouselSlides } from "./data/carouselSlides";
 import CarouselCard from "./components/CarouselCard";
 import MapButton from "../ui/buttons/MapButton";
+import ShopAddress from "../ui/shopAddress/ShopAddress";
 
 export default function CarouselHome() {
- const shopAdress = "https://www.google.com/maps/place/Serrurerie+Cordonnerie+Bourrellerie+des+Yvelines+-+Atelier+des+Frères+d'Antan/@48.7765017,1.8048008,17z/data=!3m1!4b1!4m6!3m5!1s0xa6c1002cb0eabefd:0xa27ade113f066152!8m2!3d48.7764982!4d1.8073757!16s%2Fg%2F11k9yksj1p?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoJLDEwMjExNjM5SAFQAw%3D%3D"
+ 
 
   return (
     <Swiper
@@ -29,7 +30,7 @@ export default function CarouselHome() {
       {carouselSlides.map((slide, index) => (
         <SwiperSlide key={index}>
           <CarouselCard slide={slide} />
-          <MapButton destination={shopAdress} />
+          <MapButton destination={ShopAddress()} />
         </SwiperSlide>
   ))
 }
