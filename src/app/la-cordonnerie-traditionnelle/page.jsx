2d5531f04948b4b847543f7366c4e0ca3777a381
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-26 12:05:38 +0100

    feat: add carousel data and integrate it into cordonnerie page

--- src/app/la-cordonnerie-traditionnelle/page.jsx
+++ src/app/la-cordonnerie-traditionnelle/page.jsx
@@ -1,10 +1,10 @@
-import { carouselSlides } from "@/components/pages/home/data/carouselSlides";
+import { carouselSlidesCordo } from "@/components/pages/cordonnerie/data/carouselSlidesCordo";
 import Carousel from "@/components/ui/carousel/Carousel";
 
 export default function CordonnerieTraditionnelle() {
     return (
         <div className="bg-gradient-to-b from-[#0d0d0d] to-[#1a1a1a]"> 
-            <Carousel slides={carouselSlides}  />
+            <Carousel slides={carouselSlidesCordo}  />
         </div>
     );
 };
