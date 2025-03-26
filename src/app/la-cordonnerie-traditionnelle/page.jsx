cf4ebc7a2c42b350846d1b21b0212677eaca151b
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-26 15:07:08 +0100

    feat: change background color of cordonnerie page title

--- src/app/la-cordonnerie-traditionnelle/page.jsx
+++ src/app/la-cordonnerie-traditionnelle/page.jsx
@@ -3,7 +3,7 @@ import Carousel from "@/components/ui/carousel/Carousel";
 
 export default function CordonnerieTraditionnelle() {
     return (
-        <div className="bg-gradient-to-b from-[#0d0d0d] to-[#1a1a1a] pt-32"> 
+        <div className="bg-primary pt-32"> 
             <div className="text-center my-8">
                 <h1 className="">
                    La Cordonnerie Traditionnelle
