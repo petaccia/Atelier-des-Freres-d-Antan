9416f07667a50d234228a33ea478a0f98dee0aa1
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-26 09:51:39 +0100

    feat: added sole

--- src/components/pages/cordonnerie/shoes-details/Annotations.jsx
+++ src/components/pages/cordonnerie/shoes-details/Annotations.jsx
@@ -10,6 +10,15 @@ export default function Annotations() {
                   <div className=" bg-white border-white border-1 w-10"></div>
               </div>
           </div>
+          {/* Annotations pour la semelle */}
+          <div className="absolute top-[90%] left-[40%] transform -translate-x-1/2 -translate-y-1/2">
+              <div className="flex items-center gap-4">
+                  <span className="text-white rounded-full"> 2 </span>
+                  <span className="text-white"> - </span>
+                  <span className="text-white"> Semelle </span>
+                  <div className="absolute bottom-[200%] left-[100%] bg-white border-white border-1 w-20 transform rotate-135"></div>
+              </div>
+          </div>
           </>
   )
 }
