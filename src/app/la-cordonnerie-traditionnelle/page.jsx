66aee4de889796d8b5ab2f4368df049b833f08ce
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-26 07:54:16 +0000

    feat: Created page cordonnerie and shoe details component - added image shoe details in public folder - created page cordonnerie - Created shoe details in component/pages/cordonnrie/shoes-details/shoeDetails.jsx

---
+++ src/app/la-cordonnerie-traditionnelle/page.jsx
@@ -0,0 +1,9 @@
+import ShoeDetails from "@/components/pages/cordonnerie/shoes-details/ShoeDetails";
+
+export default function CordonnerieTraditionnelle() {
+    return (
+        <div className="bg-gradient-to-b from-[#0d0d0d] to-[#1a1a1a]"> 
+            <ShoeDetails />
+        </div>
+    );
+};
--- src/components/pages/cordonnerie/shoes-details/ShoeDetails.jsx
+++ src/components/pages/cordonnerie/shoes-details/ShoeDetails.jsx
@@ -1,7 +1,18 @@
+"use client";
+import Image from "next/image";
+import shoeDetails from "../../../../../public/img/cordonnerie/shoe-details.jpg";
+
 export default function ShoeDetails() {
     return (
-        <div>
-            <h1>ShoeDetails</h1>
+        <div className="relative w-screen h-screen">
+            {/* overlay sur l'image */}
+            <div className="absolute w-full h-full bg-black opacity-50"></div>
+            <Image 
+                src={shoeDetails}
+                alt="Shoe details"
+                fill
+                className="object-contain shadow-2xl rounded-lg" 
+            />
         </div>
-    )
+    );
 }
