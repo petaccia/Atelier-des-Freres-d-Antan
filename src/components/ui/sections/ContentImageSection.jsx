bdc4537e9039203feddde1f4b52ddb09cd543df5
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-28 10:42:13 +0100

    refactor: create generic ContentImageSection component

---
+++ src/components/ui/sections/ContentImageSection.jsx
@@ -0,0 +1,62 @@
+"use client";
+import Image from "next/image";
+import GenericButton from "@/components/ui/buttons/GenericButton";
+
+export default function ContentImageSection({
+  bgColor,
+  textColor,
+  title,
+  imageSrc,
+  imageAlt,
+  content,
+  services,
+  buttonText,
+  imagePosition,
+}) {
+  return (
+    <section className={`py-20 ${bgColor}`}>
+      <div className="container mx-auto px-4">
+        <h2 className={
+          `text-center 
+          ${textColor === "text-white" ? "text-whiteAmber" : "text-primary"} }
+          mb-12`
+          }
+        >
+          {title}
+        </h2>
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-12 items-center ${imagePosition === "right" ? "md:flex-row-reverse" : ""}`}>
+          {/* Image avec bordure, ombre et effet de survol */}
+          <div className="relative h-64 md:h-96 rounded-2xl overflow-hidden group border-2 border-white shadow-lg shadow-accent/50"> {/* Bordure et ombre */}
+            <Image
+              src={imageSrc}
+              alt={imageAlt}
+              fill
+              className="object-cover transform group-hover:scale-105 transition-transform duration-500"
+            />
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black/20 group-hover:bg-black/30 transition-colors duration-500" />
+          </div>
+          {/* Contenu */}
+          <div className="space-y-6">
+            <p className="text-lg sm:text-xl text-whiteGray leading-relaxed">
+              {content}
+            </p>
+            <ul className="space-y-4">
+              {services.map ((service, index) => (
+                <li key={index} className="flex items-start">
+                  <span className="text-accent text-2xl mr-4">✓</span>
+                  <span className="text-whiteGray text-lg sm:text-xl">{service}</span>
+                </li>
+              ))}  
+            </ul>
+            <div className="mt-8">
+              <GenericButton className="px-10 py-4 bg-accent text-white rounded-full border-2 border-white font-semibold hover:bg-primary transition-colors transform hover:scale-105 active:scale-95">
+                {buttonText}
+              </GenericButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+} 
