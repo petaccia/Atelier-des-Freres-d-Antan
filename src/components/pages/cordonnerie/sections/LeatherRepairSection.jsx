0a516c26fdc18aab22943cdae9c327b925bf1588
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-28 11:47:59 +0100

    feat: add leatherRepairData and update LeatherepairSection with generic ContentImageSection

--- src/components/pages/cordonnerie/sections/LeatherRepairSection.jsx
+++ src/components/pages/cordonnerie/sections/LeatherRepairSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import ContentImageSection from "@/components/ui/sections/ContentImageSection";
-import { LeatherRepairData } from "../data/LeatherRepairData";
+import { LeatherRepairData } from "../data/leatherRepairData";
 
 export default function LeatherRepairSection() {
   return <ContentImageSection {...LeatherRepairData} />;
