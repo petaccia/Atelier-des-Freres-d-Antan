10ff6ffe492aea3993d62e0b59a5293a21bf2580
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-28 15:04:07 +0100

    Create bourrellerie page

---
+++ src/app/la-bourrellerie/page.jsx
@@ -0,0 +1,11 @@
+export default function BourrelleriePage() {
+  return (
+    <div className="bg-primary pt-32"> 
+            <div className="text-center my-8">
+                <h1 className="">
+                   La bourrellerie
+                </h1>
+            </div>
+            </div>
+  );
+}
