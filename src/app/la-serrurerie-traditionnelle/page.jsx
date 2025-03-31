349ef3c9ee14aa5a2a94dc7d4c871dfdceb780bf
petaccia <100707989+petaccia@users.noreply.github.com>
2025-03-31 12:57:15 +0200

    feat: add serrurerie page

---
+++ src/app/la-serrurerie-traditionnelle/page.jsx
@@ -0,0 +1,11 @@
+export default function LaSerrureriePage() {
+  return (
+    <div className="bg-primary pt-32"> 
+            <div className="text-center my-8">
+                <h1 className="">
+                    La serrurerie traditionnelle
+                  </h1>
+            </div>
+    </div>
+  );
+}
