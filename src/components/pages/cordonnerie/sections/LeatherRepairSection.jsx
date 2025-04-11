7790dce2678595b195c3ff0dd55ed85ab16f9b99
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-11 11:26:09 +0200

    Update branch main

--- src/components/pages/cordonnerie/sections/LeatherRepairSection.jsx
+++ src/components/pages/cordonnerie/sections/LeatherRepairSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import ContentImageSection from "@/components/ui/sections/ContentImageSection";
-import { leatherRepairData } from "../data/leatherRepairData";
+import { leatherRepairData } from "../data/LeatherRepairData";
 
 export default function LeatherRepairSection() {
   return <ContentImageSection {...leatherRepairData} id="leather-repair" />;
