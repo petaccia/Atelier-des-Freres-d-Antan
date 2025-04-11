73230bedfd7e59326e9c045f4cc3b3f483c9e936
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-11 15:05:58 +0200

    update nesxtConfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["fonts.googleapis.com"],
-  },
-  // Optimisation pour les polices
-  optimizeFonts: true,
-  // Configuration pour les assets statiques
-  staticPageGenerationTimeout: 120,
-  // Configuration pour Vercel
-  experimental: {
-    optimizeCss: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
