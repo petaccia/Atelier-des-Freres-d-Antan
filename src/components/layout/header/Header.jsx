d7ea8457052c05eaf8659d19ece80cf607facfd8
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-13 17:36:06 +0200

    Update form files

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -7,19 +7,14 @@ import Image from "next/image";
 import logo from "../../../../public/logo/logo-blue.svg";
 
 export default function Header() {
-
   return (
     <header className="fixed top-0 w-full bg-primary shadow-md text-white z-10">
       <div className="container mx-auto flex flex-col lg:flex-row py-4 justify-around items-center">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={500}
-          height={500}
-          className="w-[150px] h-[150px]"
-        />
+        <Image src={logo} alt="Logo" width={500} height={500} className="w-[150px] h-[150px]" />
         <Link href="/">
-            <h1 className="hidden md:block text-xl pt-4 md:mt-0 mb-4 lg:mb-0">Atelier des Frères d'Antan</h1>
+          <h1 className="hidden md:block text-xl pt-4 md:mt-0 mb-4 lg:mb-0">
+            Atelier des Frères d'Antan
+          </h1>
         </Link>
 
         {/* Menu Desktop */}
--- src/components/ui/pageTitle/PageTitle.jsx
+++ src/components/ui/pageTitle/PageTitle.jsx
@@ -6,9 +6,7 @@ export default function PageTitle({
   titleClassName = "text-center text-white rounded-lg",
 }) {
   return (
-    <div
-      className={`"text-center mt-20 my-4 md:my-8" ${className}`}
-    >
+    <div className={`"text-center mt-20 my-4 md:my-8" ${className}`}>
       <h1 className={titleClassName}>{title}</h1>
     </div>
   );
