6726122bfd614a7b390dab61b2970e2b8605022f
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-17 15:36:30 +0200

    fix(backend): correct API route by removing duplicate api prefix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health/health.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AdminModule } from './admin/admin.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [PrismaModule, AdminModule],
+  imports: [PrismaModule, AdminModule, MenuModule],
   controllers: [HealthController],
   providers: [],
 })
