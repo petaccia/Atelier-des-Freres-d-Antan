bc22549fffab377d0aeb5158f380504a7554852c
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-22 09:37:16 +0200

    Feature: Add CRUD API for menu items in backend

--- backend/src/menu/menu.controller.ts
+++ backend/src/menu/menu.controller.ts
@@ -1,14 +1,40 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UseInterceptors, ParseIntPipe, HttpCode } from '@nestjs/common';
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { MenuService } from './menu.service';
+import { CreateMenuItemDto } from './dto/create-menu-item.dto';
+import { UpdateMenuItemDto } from './dto/update-menu-item.dto';
 
 @Controller('menu')
-@UseInterceptors(CacheInterceptor)
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
   @Get('main')
+  @UseInterceptors(CacheInterceptor)
   async getMainMenu() {
     return this.menuService.getMainMenu();
+  }
+
+  @Get('items/:id')
+  async getMenuItem(@Param('id', ParseIntPipe) id: number) {
+    return this.menuService.getMenuItem(id);
+  }
+
+  @Post('items')
+  async createMenuItem(@Body() createMenuItemDto: CreateMenuItemDto) {
+    return this.menuService.createMenuItem(createMenuItemDto);
+  }
+
+  @Put('items/:id')
+  async updateMenuItem(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateMenuItemDto: UpdateMenuItemDto,
+  ) {
+    return this.menuService.updateMenuItem(id, updateMenuItemDto);
+  }
+
+  @Delete('items/:id')
+  @HttpCode(200)
+  async deleteMenuItem(@Param('id', ParseIntPipe) id: number) {
+    return this.menuService.deleteMenuItem(id);
   }
 }
