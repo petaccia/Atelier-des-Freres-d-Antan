577e17ddf394f8fc9175ab4129009ee290149c81
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-24 12:12:45 +0200

    added component title

--- frontend/src/app/backoffice/site/menu/page.jsx
+++ frontend/src/app/backoffice/site/menu/page.jsx
@@ -1,14 +1,13 @@
 "use client";
 import { useState } from 'react';
 import Sidebar from '@/backoffice/components/layouts/Sidebar';
-import Link from 'next/link';
 import DeviceSelector from '@/components/ui/selectors/DeviceSelector';
-import { MenuItem, useDeviceMenu } from '@/backoffice/components/menu';
-import LoadingState from '@/backoffice/components/ui/loading/LoadingState';
-import ErrorState from '@/backoffice/components/ui/error/ErrorState';
+import { useDeviceMenu } from '@/backoffice/components/menu';
 import MenuItemRecursive from '@/backoffice/components/menu/components/recursive/MenuItemRecursive';
 import PageLoading from "@/backoffice/components/layouts/PageLoading";
 import PageError from "@/backoffice/components/layouts/PageError";
+import BackofficeHeaderLocationPage from "@/backoffice/components/navigation/BackofficeHeaderLocationPage";
+import BackofficeHeaderTitle from "@/backoffice/components/navigation/BackofficeHeaderTitle";
 
 export default function MenuPage() {
   const [selectedDevice, setSelectedDevice] = useState('mobile');
@@ -28,18 +27,16 @@ export default function MenuPage() {
     <div className="min-h-screen bg-primary">
       <Sidebar />
       <div className="ml-64 p-8">
-        {/* Titre et navigation */}
-        <div className="flex items-center gap-2 text-white/60 mb-4">
-          <Link href="/backoffice/site" className="hover:text-accent">
-            Site Internet
-          </Link>
-          <span>/</span>
-          <span className="text-accent">Menu</span>
-        </div>
-
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-2xl font-bold text-whiteAmber">Gestion du Menu</h1>
-        </div>
+        <BackofficeHeaderLocationPage 
+        parentLink={{
+          href: "/backoffice/site",
+          label: "Site Internet"
+        }}
+        currentPage="Menu"
+        />
+        <BackofficeHeaderTitle 
+          title="Gestion du Menu" 
+        />
 
         {/* Sélecteur d'appareil */}
         <div className="mb-6">
@@ -51,7 +48,7 @@ export default function MenuPage() {
 
         {/* Affichage des éléments du menu */}
         <div className="bg-primary-dark/30 p-4 rounded-lg">
-          <h2 className="text-xl text-white mb-4">Éléments du menu {selectedDevice}</h2>
+          <h2 className="text-xl text-white">Éléments du menu {selectedDevice}</h2>
 
           {selectedDevice !== 'desktop' ? (
             <div className="space-y-4">
