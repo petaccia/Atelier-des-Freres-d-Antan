86ffd6a316c650cd9a60e695b7251c7f3c04bdbc
petaccia <100707989+petaccia@users.noreply.github.com>
2025-04-24 18:51:13 +0200

    fix: add missing controllers and services in MenuModule

--- backend/src/menu/menu.module.ts
+++ backend/src/menu/menu.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { MenuController } from './menu.controller';
 import { MenuService } from './services/menu.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { DesktopMenuController } from './desktop/desktop-menu.controller';
+import { MobileMenuController } from './mobile/mobile-menu.controller';
+import { DesktopMenuService } from './desktop/services/desktop-menu.service';
+import { MobileMenuService } from './mobile/services/mobile-menu.service';
 
 @Module({
-  controllers: [MenuController],
-  providers: [MenuService, PrismaService],
-  exports: [MenuService],
+  controllers: [MenuController, DesktopMenuController, MobileMenuController ],
+  providers: [MenuService, PrismaService, DesktopMenuService, MobileMenuService],
+  exports: [MenuService, DesktopMenuService, MobileMenuService],
 })
 export class MenuModule {}
