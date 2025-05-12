6347e5a4b0a6f6152b45f55bd85142041ee1939c
petaccia <100707989+petaccia@users.noreply.github.com>
2025-05-12 16:27:31 +0200

    fix: update media page layout
       - Add BeforeAfterCard component to media page
       - Adjust layout and spacing for better UI consistency

--- frontend/src/app/backoffice/site/medias/page.jsx
+++ frontend/src/app/backoffice/site/medias/page.jsx
@@ -1,15 +1,26 @@
+"use client";
+import BackofficeLayout from "@/backoffice/components/layouts/BackofficeLayout";
+import BackofficeHeaderLocationPage from "@/backoffice/components/navigation/BackofficeHeaderLocationPage";
+import BackofficeHeaderTitle from "@/backoffice/components/navigation/BackofficeHeaderTitle";
 import BeforeAfterCard from "@/backoffice/ui/cards/BeforeAfterCard";
 
 const PageMedia = () => {
   return (
-    <div>
-      <h1>Page de gestion des médias</h1>
+    <BackofficeLayout>
+      <BackofficeHeaderLocationPage
+        parentLink={{
+          href: "/backoffice/site",
+          label: "Site Internet",
+        }}
+        currentPage="Médias"
+      />
+      <BackofficeHeaderTitle title="Gestion des Médias" />
       
     <BeforeAfterCard 
       title="Atelier Des Frères d'Antan"
       className="mt-8"
        />
-      </div>
+      </BackofficeLayout>
   )
 }
 
